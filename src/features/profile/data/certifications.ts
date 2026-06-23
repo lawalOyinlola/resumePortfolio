50fe30c3bdd1275b82a7e873c7da72d37709bb70
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-06-23 13:37:49 +0000

    feat: use issuer logos for certifications

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -3,8 +3,8 @@ import type { Certification } from "../types/certifications";
 export const CERTIFICATIONS: Certification[] = [
   {
     title: "The Complete JavaScript Course 2025: From Zero to Expert!",
-    issuer: "Jonas Schmedtmann",
-    issuerIconName: "udemy",
+    issuer: "Udemy | Jonas Schmedtmann",
+    issuerLogoURL: "https://icons.duckduckgo.com/ip3/udemy.com.ico",
     issueDate: "2026-06-10",
     credentialID: "UC-c272f647-09ce-4317-8091-4dd141679953",
     credentialURL:
@@ -13,7 +13,7 @@ export const CERTIFICATIONS: Certification[] = [
   {
     title: "HNG Internship 12 (2025) — Frontend Development Track Finalist",
     issuer: "HNG Internship",
-    issuerIconName: "hng",
+    issuerLogoURL: "https://hng.tech/favicon.ico",
     issueDate: "2025-04",
     credentialID: "HNG12-FE-Finalist",
     credentialURL:
@@ -22,28 +22,28 @@ export const CERTIFICATIONS: Certification[] = [
   {
     title: "AltSchool Africa Internship Cohort 2024",
     issuer: "AltSchool Africa",
-    issuerIconName: "altschool",
+    issuerLogoURL: "/images/altschool-logo.webp",
     issueDate: "2024-03",
-    credentialID: "AltSchool-2024-Graduation",
+    credentialID: "REF-w1MDbp7Y8djHKuqm38",
     credentialURL:
       "https://drive.google.com/file/d/13hZF7tc02X0W0AG4OxsrYC9HPwKfofmL/view?usp=drive_link",
   },
   {
     title: "The Ultimate React Course 2024: React, Next.js, Redux & More",
-    issuer: "Jonas Schmedtmann",
-    issuerIconName: "udemy",
+    issuer: "Udemy | Jonas Schmedtmann",
+    issuerLogoURL: "https://icons.duckduckgo.com/ip3/udemy.com.ico",
     issueDate: "2024-12",
-    credentialID: "Ultimate-React-2024",
+    credentialID: "UC-908f74c2-471c-4759-97dc-ca875480778f",
     credentialURL:
       "https://drive.google.com/file/d/1atVgRIBoo30zj0kwVbgp7IyaS4zZPYdE/view?usp=drive_link",
   },
   {
     title:
       "Learning How to Learn: Powerful mental tools to help you master tough subjects",
-    issuer: "Coursera",
+    issuer: "Coursera | Deep Teaching Solutions",
     issuerIconName: "coursera",
     issueDate: "2024-11",
-    credentialID: "Learning-How-to-Learn",
+    credentialID: "C5LGZQNP2BYP",
     credentialURL:
       "https://drive.google.com/file/d/1wXRdQLKmr7GGg6RWQGktzfgAL8nZXU1e/view?usp=drive_link",
   },
